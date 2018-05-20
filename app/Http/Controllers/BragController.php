f17b84d8d2c4775da013fa8e3235b29d9ca4ac75
Michael Whang <mike@tinybot.ca>
2018-05-20 15:57:38 -0700

    Show random brag on welcome page

--- app/Http/Controllers/BragController.php
+++ app/Http/Controllers/BragController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Brag;
 
 class BragController extends Controller
 {
-    //
+    public function index()
+    {
+        $brag = Brag::all();
+
+        // dd($brag);
+
+        return view('welcome', compact('brag'));
+    }
 }
